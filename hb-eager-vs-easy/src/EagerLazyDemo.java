a2c6284bf09985f9ac26637b2ad856bc56fd0c01
Le Quang Duy <43492067+Iamthankyou@users.noreply.github.com>
2020-03-31 09:12:38 +0700

    option 2: use HQL

--- hb-eager-vs-easy/src/EagerLazyDemo.java
+++ hb-eager-vs-easy/src/EagerLazyDemo.java
@@ -1,6 +1,7 @@
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
 
 import com.luv2code.demo.entity.Course;
 import com.luv2code.demo.entity.Instructor;
@@ -19,13 +20,15 @@ public class EagerLazyDemo {
 			int id =1;
 			
 			System.out.println("Getting...");
-			Instructor tmpInstructor = session.get(Instructor.class, id);			
 			
-			System.out.println("Duy:Instructor: "+tmpInstructor);
-			System.out.println("Duy:Course of Instructor: " + tmpInstructor.getCourse());
+			// Option 2: Hibernate query with HQL
+			Query<Instructor> query = session.createQuery("select i from Instructor i JOIN FETCH i.course where i.id=:theInstructorId");
+			// Set parameter query
+			query.setParameter("theInstructorId", id);
+			// Executed query & get Instructor
+			Instructor tmpInstructor = query.getSingleResult();
 			
 			session.getTransaction().commit();
-			
 			session.close();
 			System.out.println("Session closed!");
 			
