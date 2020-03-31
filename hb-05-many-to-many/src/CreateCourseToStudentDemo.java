b28044fe009d8ee0b4cb508132cf0b4c82ab2af3
Le Quang Duy <43492067+Iamthankyou@users.noreply.github.com>
2020-03-31 21:24:42 +0700

    add more course to student Duy

--- hb-05-many-to-many/src/CreateCourseToStudentDemo.java
+++ hb-05-many-to-many/src/CreateCourseToStudentDemo.java
@@ -9,7 +9,7 @@ import com.luv2code.demo.entity.InstructorDetail;
 import com.luv2code.demo.entity.Reviewer;
 import com.luv2code.demo.entity.Student;
 
-public class CreateCourseStudentDemo {
+public class CreateCourseToStudentDemo {
 
 	public static void main(String[] args) {
 		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Instructor.class).addAnnotatedClass(InstructorDetail.class).addAnnotatedClass(Course.class).addAnnotatedClass(Reviewer.class).addAnnotatedClass(Student.class).buildSessionFactory();
@@ -19,25 +19,14 @@ public class CreateCourseStudentDemo {
 		try {
 			session.beginTransaction();
 			
-			// Create new course and new student
-
-			Course tmpCourse = new Course("Learning Java C++");
+			int id =1;
+			Student tmpStudent = session.get(Student.class,id);
 			
-			System.out.println("Saving course");
+			Course tmpCourse = new Course("Learnning AI");
+			tmpCourse.addStudent(tmpStudent);
+			
+			System.out.println("Saving");
 			session.save(tmpCourse);
-			
-			Student tmpStudent1 = new Student("Le", "Duy", "quangduynb00@gmail.com");
-			Student tmpStudent2 = new Student("Pham", "Hien", "rongkonchemgio@gmail.com");
-			
-			tmpCourse.addStudent(tmpStudent1);
-			tmpCourse.addStudent(tmpStudent2);
-			
-			System.out.println("Saving student");
-			session.save(tmpStudent1);
-			session.save(tmpStudent2);
-			
-			// Add reviewer
-			tmpCourse.add(new Reviewer("This course is coool"));
 			
 			session.getTransaction().commit();
 			System.out.println("Done!");
