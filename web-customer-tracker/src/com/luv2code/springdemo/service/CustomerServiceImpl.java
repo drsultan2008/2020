53a20e6ee4f7742bfff0e047a5f64f59133a58ea
Le Quang Duy <43492067+Iamthankyou@users.noreply.github.com>
2020-04-03 19:53:05 +0700

    SQLite program

---
+++ web-customer-tracker/src/com/luv2code/springdemo/service/CustomerServiceImpl.java
@@ -0,0 +1,24 @@
+package com.luv2code.springdemo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.luv2code.springdemo.dao.CustomerDAO;
+import com.luv2code.springdemo.entity.Customer;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+	
+	@Autowired
+	private CustomerDAO customerDAO;
+	
+	@Override
+	@Transactional
+	public List<Customer> getCustomers() {
+		return customerDAO.getCustomers();
+	}
+
+}
