cc52ae4c8834d290f3336dc00cd7c5b9ac93c867
Le Quang Duy <43492067+Iamthankyou@users.noreply.github.com>
2020-04-03 12:00:32 +0700

    RequestMapping -> Getmapping
    GetCustomer in Controller class is GET

--- web-customer-tracker/src/com/luv2code/springdemo/controller/CustomerController.java
+++ web-customer-tracker/src/com/luv2code/springdemo/controller/CustomerController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.luv2code.springdemo.dao.CustomerDAO;
@@ -18,7 +20,7 @@ public class CustomerController {
 	@Autowired
 	private CustomerDAO customerDAO ;
 	
-	@RequestMapping("/list")
+	@GetMapping("/list")
 	public String listCustomers(Model model) {
 		
 		List<Customer> customers = customerDAO.getCustomers();
