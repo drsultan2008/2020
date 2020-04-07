2e0c302ea7b6a5da3550c36e2c8ba48060dbc542
Le Quang Duy <43492067+Iamthankyou@users.noreply.github.com>
2020-04-07 12:49:39 +0700

    @After (finally) method

---
+++ spring-demo-aop-after-finally/src/com/luv2code/aopdemo/MainDemoApp.java
@@ -0,0 +1,36 @@
+package com.luv2code.aopdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.luv2code.aopdemo.dao.AccountDAO;
+import com.luv2code.aopdemo.dao.MembershipDAO;
+
+public class MainDemoApp {
+
+	public static void main(String[] args) {
+		// Read spring config class
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DemoConfig.class);
+		// Get the bean from string container
+		AccountDAO accountDAO = context.getBean("accountDAO",AccountDAO.class);
+		MembershipDAO membershipDAO = context.getBean("membershipDAO",MembershipDAO.class);
+		// Call the business method
+		Account theAccount = new Account();
+		theAccount.setLevel("Name Level");
+		theAccount.setName("Name name");
+		
+		accountDAO.addAccount(theAccount,true);
+		membershipDAO.addSillyMember();
+		accountDAO.addOther();
+		
+		accountDAO.setName("name");
+		accountDAO.setServiceCode("service");
+		
+		String name = accountDAO.getName();
+		String service = accountDAO.getServiceCode();
+		
+		// Close context
+		context.close();
+		
+	}
+
+}
