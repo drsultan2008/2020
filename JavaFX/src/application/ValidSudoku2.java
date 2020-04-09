1718361d826f1aa7298ae6f520c992da86a13037
Le Quang Duy <43492067+Iamthankyou@users.noreply.github.com>
2020-04-09 15:03:52 +0700

    valid sudoku short

---
+++ JavaFX/src/application/ValidSudoku2.java
@@ -0,0 +1,41 @@
+package application;
+
+import java.util.HashSet;
+
+public class ValidSudoku2 {
+	
+	public static boolean validSudoku(char[][] board) {
+		HashSet<Character> rowSet = new HashSet<>();
+		HashSet<Character> colSet = new HashSet<>();
+		HashSet<Character> blockSet = new HashSet<>();
+		
+		for (int i=0; i<9; i++) {
+			for (int j=0; j<9; j++) {
+				if (board[i][j]=='.' || !rowSet.add(board[i][j])) return false;
+				if (board[j][i]=='.' || !colSet.add(board[j][i])) return false;
+				int rowBlock = i/3*3 + j/3;
+				int colBlock = i%3*3 + j%3;
+				System.out.println(rowBlock+","+colBlock+" ");
+				if (board[rowBlock][colBlock]=='i'|| !blockSet.add(board[rowBlock][colBlock])) return false;
+			}
+		}
+		return true;
+	}
+	
+	public static void main(String[] args) {
+		ValidSudoku2 sudoku = new ValidSudoku2();
+		String input = "974236158638591742125487936316754289742918563589362417867125394253649871491873625";
+		char board[][] = new char[9][9];
+		int iter = 0;
+		
+		for (int i=0; i<9; i++) {
+			for (int j=0; j<9; j++) {
+				board[i][j]=input.charAt(iter++);
+			}
+		}
+		
+		System.out.println(validSudoku(board));
+		
+	}
+
+}
