a14d51d5d5e3cfcf446578ef57c45c46f8c0efb0
Le Quang Duy <43492067+Iamthankyou@users.noreply.github.com>
2020-04-11 11:51:17 +0700

    custom form spring @security

---
+++ spring-security-demo-02-custom-form/src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
@@ -0,0 +1,27 @@
+package com.luv2code.springsecurity.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+@Configuration
+@EnableWebSecurity
+public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		UserBuilder user = User.withDefaultPasswordEncoder();
+		auth.inMemoryAuthentication().withUser(user.username("duy").password("duyduy").roles("ADMIN"))
+		.withUser(user.username("pro").password("propro").roles("EMPLOYEE"));
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().anyRequest().authenticated().and().formLogin().loginPage("/showFormLogin").loginProcessingUrl("/authenticateTheUser").permitAll();
+	}
+	
+}
---
+++ spring-security-demo-02-custom-form/src/main/java/com/luv2code/springsecurity/demo/controller/LoginController.java
@@ -0,0 +1,12 @@
+package com.luv2code.springsecurity.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+	@GetMapping("/showFormLogin")
+	public String showFormLogin(){
+		return "plain-form";
+	}
+}
