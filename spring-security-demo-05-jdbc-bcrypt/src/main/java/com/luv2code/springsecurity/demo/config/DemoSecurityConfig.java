b002a2f0b9d03e6448ac30ecf423d50822aa1582
Le Quang Duy <43492067+Iamthankyou@users.noreply.github.com>
2020-04-13 00:12:35 +0700

    bcrypt database security

---
+++ spring-security-demo-05-jdbc-bcrypt/src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
@@ -0,0 +1,38 @@
+package com.luv2code.springsecurity.demo.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+@Configuration
+@EnableWebSecurity
+public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private DataSource securityDataSource;
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// Use jdbc authentication
+		auth.jdbcAuthentication().dataSource(securityDataSource);
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.antMatchers("/").hasRole("EMPLOYEE")
+		.antMatchers("/leaders/**").hasRole("ADMIN")
+		.antMatchers("/system/**").hasRole("MANAGER")
+		.and().formLogin().loginPage("/showFormLogin").loginProcessingUrl("/authenticateTheUser").permitAll()
+		.and().logout().permitAll()
+		.and().exceptionHandling().accessDeniedPage("/denied");
+	}
+	
+}
