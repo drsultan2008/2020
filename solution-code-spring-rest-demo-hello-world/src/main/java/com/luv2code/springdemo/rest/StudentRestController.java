f59b5b8fa879e46c32dff5f6eca1601486a71719
Le Quang Duy <43492067+Iamthankyou@users.noreply.github.com>
2020-04-15 22:59:44 +0700

    Exception handling generic

--- solution-code-spring-rest-demo-hello-world/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
+++ solution-code-spring-rest-demo-hello-world/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
@@ -56,6 +56,15 @@ public class StudentRestController {
 		return new ResponseEntity<>(error,HttpStatus.NOT_FOUND);
 	}
 	
-	
+	@ExceptionHandler
+	public ResponseEntity<StudentErrorResponse> handleException(Exception exc){
+		StudentErrorResponse error = new StudentErrorResponse();
+		
+		error.setStatus(HttpStatus.BAD_REQUEST.value());
+		error.setMessage(exc.getMessage());
+		error.setTimeStamp(System.currentTimeMillis());
+		
+		return new ResponseEntity<>(error,HttpStatus.BAD_REQUEST);
+	}
 	
 }
