67cd2bb19a01bd3a545f7fd11dc5eee17304e284
Le Quang Duy <43492067+Iamthankyou@users.noreply.github.com>
2020-04-15 23:10:55 +0700

    Practice expect exception handling with @ControllerAdvice

--- solution-code-spring-rest-demo-hello-world/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
+++ solution-code-spring-rest-demo-hello-world/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
@@ -43,28 +43,4 @@ public class StudentRestController {
 		return students.get(studentId);
 	}
 	
-	// Add an exception handler using @Exceptionhandler
-	@ExceptionHandler
-	public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc){
-		// Create a StudentErrorResponse
-		StudentErrorResponse error = new StudentErrorResponse();
-		
-		error.setStatus(HttpStatus.NOT_FOUND.value());
-		error.setMessage(exc.getMessage());
-		error.setTimeStamp(System.currentTimeMillis());
-		// Return ResponseEntity
-		return new ResponseEntity<>(error,HttpStatus.NOT_FOUND);
-	}
-	
-	@ExceptionHandler
-	public ResponseEntity<StudentErrorResponse> handleException(Exception exc){
-		StudentErrorResponse error = new StudentErrorResponse();
-		
-		error.setStatus(HttpStatus.BAD_REQUEST.value());
-		error.setMessage(exc.getMessage());
-		error.setTimeStamp(System.currentTimeMillis());
-		
-		return new ResponseEntity<>(error,HttpStatus.BAD_REQUEST);
-	}
-	
 }
