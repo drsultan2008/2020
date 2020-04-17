03275e8288169d58fc96da1ca4542cec47ff418b
Le Quang Duy <43492067+Iamthankyou@users.noreply.github.com>
2020-04-17 22:28:08 +0700

    Add inject @Value with properties custom

--- springboot-mycoolapp/springboot-mycoolapp/src/main/java/com/luv2code/springboot/demo/springbootmycoolapp/rest/FunRestController.java
+++ springboot-mycoolapp/springboot-mycoolapp/src/main/java/com/luv2code/springboot/demo/springbootmycoolapp/rest/FunRestController.java
@@ -2,11 +2,18 @@ package com.luv2code.springboot.demo.springbootmycoolapp.rest;
 
 import java.time.LocalDate;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+	// Inject value form properties
+	@Value("${coach.name}")
+	private String coachName;
+	@Value("${team.name}")
+	private String teamName;
+	
 	@GetMapping("/")
 	public String sayHello() {
 		return "Hello World ! Time on server is " + LocalDate.now();
@@ -17,8 +24,13 @@ public class FunRestController {
 		return "Hello daily workout!";
 	}
 	
-	@GetMapping("fortune")
+	@GetMapping("/fortune")
 	public String sayFortune() {
 		return "Hello daily fortune";
+	}
+	
+	@GetMapping("/teaminfo")
+	public String getTeamInfo() {
+		return coachName + " " + teamName;
 	}
 }
