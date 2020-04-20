965b43d462d3e574eda22f0d6486d1a6dd454c63
Iamthankyou <quangduynb00@gmail.com>
2020-04-20 21:15:46 +0700

    Hello thymeleaf with css & html

---
+++ thymeleafdemo/src/main/java/luv2code/springboot/thymeleafdemo/controller/DemoController.java
@@ -0,0 +1,14 @@
+package luv2code.springboot.thymeleafdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+	@GetMapping("/hello")
+	public String sayHello(Model model) {
+		model.addAttribute("date", new java.util.Date());
+		return "helloworld";
+	}
+}
